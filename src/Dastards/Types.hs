aee81b659aa8cee284b864c09df2273a3a544f93
David <dmn@Davids-MacBook-Pro.local>
2018-03-24 00:21:58 -0400

    banishing the dastard

--- src/Dastards/Types.hs
+++ src/Dastards/Types.hs
@@ -2,19 +2,21 @@ module Dastards.Types where
 
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+
 
 type Money = Int
 data ScholarType = Scientist | Doctor | Priest | Clerk
-    deriving (Show, Eq)
+    deriving (Show, Eq, Ord, Read)
 
 data Player = Blue | Yellow | Green | Red | Violet
-    deriving (Show, Eq, Ord)
+    deriving (Show, Eq, Ord, Read)
 
-data SalArea = SalArea (Maybe(Scholar))
-    deriving (Show, Eq)
+type SalArea = Maybe(Scholar)
 
 data Palace = Palace SalArea SalArea SalArea SalArea
-    deriving (Show, Eq)
+    deriving (Show, Eq, Read)
 
 type Island = [Scholar]
 
@@ -22,18 +24,18 @@ type Scholar = (Player, ScholarType)
 
 data PlayerState = PlayerState
     { psMoney :: Money
-    , psReserveScholars :: [Scholar]
+    , psReserveScholars :: Set Scholar
     , psPalace :: Palace
-    , psApplications :: [Scholar]
+    , psApplications :: Set Scholar
     }
-    deriving (Show, Eq)
+    deriving (Show, Eq, Read)
 data GameState = GameState
     { gsPlayerStates :: Map Player PlayerState
     , gsIsland :: Island
     , gsCurrentPlayer :: Player
     , gsTurnCount :: Int
     }
-    deriving (Show, Eq)
+    deriving (Show, Eq, Read)
 
 
 data Move
@@ -44,14 +46,14 @@ data Move
 
 
 initialSalArea :: SalArea
-initialSalArea = SalArea Nothing
+initialSalArea = Nothing
 
 initialPlayerState :: Player -> PlayerState
 initialPlayerState pl = PlayerState
     { psMoney = 32
-    , psReserveScholars = zip (repeat pl) [Scientist, Scientist, Doctor, Doctor, Priest, Priest, Clerk, Clerk]
+    , psReserveScholars = Set.fromList $ zip (repeat pl) [Scientist, Scientist, Doctor, Doctor, Priest, Priest, Clerk, Clerk]
     , psPalace = Palace initialSalArea initialSalArea initialSalArea initialSalArea
-    , psApplications = []
+    , psApplications = Set.empty
     }
 
 initialGameState :: [Player] -> GameState
@@ -62,10 +64,27 @@ initialGameState pls = GameState
     , gsTurnCount = 0
     }
 
--- makeMove :: GameState -> Move -> GameState
--- makeMove gs (Banish s) = GameState
---     { gsPlayerStates = Map.fromList $ map ( \pl -> (pl, initialPlayerState pl) ) pls
---     , gsIsland = s : gsIsland gs
---     , gsCurrentPlayer = gsCurrentPlayer gs
---     , gsTurnCount = gsTurnCount gs
---     }
+makeMove :: GameState -> Move -> GameState
+makeMove gs (Banish scholar) = GameState
+    { gsPlayerStates = Map.adjust removeScholar (gsCurrentPlayer gs) (gsPlayerStates gs)
+    , gsIsland = scholar : gsIsland gs
+    , gsCurrentPlayer = gsCurrentPlayer gs
+    , gsTurnCount = gsTurnCount gs
+    }
+    where
+        removeScholar :: PlayerState -> PlayerState
+        removeScholar pl = PlayerState
+            { psMoney = psMoney pl
+            , psReserveScholars = psReserveScholars pl
+            , psPalace = removeFromPalace ( psPalace pl )
+            , psApplications = Set.delete scholar (psApplications pl)
+            }
+        removeFromPalace :: Palace -> Palace
+        removeFromPalace (Palace s1 s2 s3 s4) = Palace (removeFromSalArea s1) (removeFromSalArea s2) (removeFromSalArea s3) (removeFromSalArea s4)
+        removeFromSalArea :: SalArea -> SalArea
+        removeFromSalArea Nothing = Nothing
+        removeFromSalArea (Just scholar1) = if scholar1 == scholar then Nothing else Just scholar1
+
+
+            -- isEqual <- (==) scholar <$> sA
+            -- if isEqual then Nothing else sA
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+import Test.Hspec
+import Dastards.Types
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "makeMove" $ do
+        it "banish from applications" $ makeMove beforeBanishFromApps (Banish (Green, Scientist)) `shouldBe` afterBanishFromApps
+        it "banish from palace" $ makeMove beforeBanishFromPal (Banish (Green, Scientist)) `shouldBe` afterBanishFromPal
+
+beforeBanishFromApps :: GameState
+beforeBanishFromApps = read "GameState {gsPlayerStates = fromList [(Green,PlayerState {psMoney = 32, psReserveScholars = fromList [(Green,Doctor),(Green,Priest),(Green,Clerk)], psPalace = Palace Nothing Nothing Nothing Nothing, psApplications = fromList []}),(Red,PlayerState {psMoney = 32, psReserveScholars = fromList [(Red,Scientist),(Red,Doctor),(Red,Priest),(Red,Clerk)], psPalace = Palace Nothing Nothing Nothing Nothing, psApplications = fromList []}),(Violet,PlayerState {psMoney = 32, psReserveScholars = fromList [(Violet,Scientist),(Violet,Doctor),(Violet,Priest),(Violet,Clerk)], psPalace = Palace Nothing Nothing Nothing Nothing, psApplications = fromList [(Green,Scientist)]})], gsIsland = [], gsCurrentPlayer = Violet, gsTurnCount = 0}"
+afterBanishFromApps :: GameState
+afterBanishFromApps = read "GameState {gsPlayerStates = fromList [(Green,PlayerState {psMoney = 32, psReserveScholars = fromList [(Green,Doctor),(Green,Priest),(Green,Clerk)], psPalace = Palace Nothing Nothing Nothing Nothing, psApplications = fromList []}),(Red,PlayerState {psMoney = 32, psReserveScholars = fromList [(Red,Scientist),(Red,Doctor),(Red,Priest),(Red,Clerk)], psPalace = Palace Nothing Nothing Nothing Nothing, psApplications = fromList []}),(Violet,PlayerState {psMoney = 32, psReserveScholars = fromList [(Violet,Scientist),(Violet,Doctor),(Violet,Priest),(Violet,Clerk)], psPalace = Palace Nothing Nothing Nothing Nothing, psApplications = fromList []})], gsIsland = [(Green, Scientist)], gsCurrentPlayer = Violet, gsTurnCount = 0}"
+
+beforeBanishFromPal :: GameState
+beforeBanishFromPal = read "GameState {gsPlayerStates = fromList [(Green,PlayerState {psMoney = 32, psReserveScholars = fromList [(Green,Doctor),(Green,Clerk)], psPalace = Palace Nothing Nothing Nothing Nothing, psApplications = fromList []}),(Red,PlayerState {psMoney = 32, psReserveScholars = fromList [(Red,Scientist),(Red,Doctor),(Red,Priest),(Red,Clerk)], psPalace = Palace Nothing Nothing Nothing Nothing, psApplications = fromList []}),(Violet,PlayerState {psMoney = 32, psReserveScholars = fromList [(Violet,Scientist),(Violet,Doctor),(Violet,Priest),(Violet,Clerk)], psPalace = Palace (Just(Green, Scientist)) Nothing Nothing (Just(Green, Priest)), psApplications = fromList []})], gsIsland = [], gsCurrentPlayer = Violet, gsTurnCount = 0}"
+afterBanishFromPal :: GameState
+afterBanishFromPal = read "GameState {gsPlayerStates = fromList [(Green,PlayerState {psMoney = 32, psReserveScholars = fromList [(Green,Doctor),(Green,Clerk)], psPalace = Palace Nothing Nothing Nothing Nothing, psApplications = fromList []}),(Red,PlayerState {psMoney = 32, psReserveScholars = fromList [(Red,Scientist),(Red,Doctor),(Red,Priest),(Red,Clerk)], psPalace = Palace Nothing Nothing Nothing Nothing, psApplications = fromList []}),(Violet,PlayerState {psMoney = 32, psReserveScholars = fromList [(Violet,Scientist),(Violet,Doctor),(Violet,Priest),(Violet,Clerk)], psPalace = Palace Nothing Nothing Nothing (Just(Green, Priest)), psApplications = fromList []})], gsIsland = [(Green, Scientist)], gsCurrentPlayer = Violet, gsTurnCount = 0}"
